95f3974b572f5cbb7c632f64ef7ed7d9235f1225
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-05-20 17:46:58 +0000

    Pushing files/folders for task 4 to see explicit checker

---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Creates a route that returns a JSON"""
+
+from flask import jsonify
+from api.v1.views import app_views
+
+@app_views.route("/status", strict_slashes=False)
+def return_jsonify():
+    """Returns JSON status=OK"""
+    return jsonify({"status": "OK"})
