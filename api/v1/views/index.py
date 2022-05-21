be4d707ae35a65349d95cf2bead96f5dd65967fa
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-05-21 14:32:04 +0000

    fixed typos

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,12 +4,6 @@
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import  User
 
 
 @app_views.route("/status", strict_slashes=False)
@@ -21,6 +15,12 @@ def return_jsonify():
 @app_views.route("/stats", strict_slashes=False)
 def endpoint():
     """Retrieves number of each object by type"""
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
     return jsonify({
         'amenities': storage.count(Amenity),
         'cities': storage.count(City),
