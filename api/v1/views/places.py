090821ddac117a5a2bb7d288ddaeab0843fd842c
andresh <ahenderson.holberton@gmail.com>
2022-05-23 11:52:55 -0300

    test 11

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -48,28 +48,27 @@ def delete_place_with_id(place_id):
     return jsonify({}), 200
 
 
-@app_views.route('/cities/<string:city_id>/places', methods=['POST'],
-                 strict_slashes=False)
-def post_place(city_id):
-    """create a new place with given city id"""
-    city = storage.get(City, city_id)
-    if city is None:
-        abort(404)
-    data = request.get_json()
-    if data is None:
-        abort(400, 'Not a JSON')
-    if 'user_id' not in data:
-        abort(400, 'Missing user_id')
-    user = storage.get("User", data['user_id'])
-    if user is None:
-        abort(404)
-    if 'name' not in data:
-        abort(400, 'Missing name')
-    data['city_id'] = city_id
-    new_place = Place(**data)
-    new_place.save()
-    return jsonify(new_place.to_dict()), 201
-
+# @app_views.route('/cities/<string:city_id>/places', methods=['POST'],
+#                  strict_slashes=False)
+# def post_place(city_id):
+#     """create a new place with given city id"""
+#     city = storage.get(City, city_id)
+#     if city is None:
+#         abort(404)
+#     data = request.get_json()
+#     if data is None:
+#         abort(400, 'Not a JSON')
+#     if 'user_id' not in data:
+#         abort(400, 'Missing user_id')
+#     user = storage.get("User", data['user_id'])
+#     if user is None:
+#         abort(404)
+#     if 'name' not in data:
+#         abort(400, 'Missing name')
+#     data['city_id'] = city_id
+#     new_place = Place(**data)
+#     new_place.save()
+#     return jsonify(new_place.to_dict()), 201
 
 # @app_views.route("/places/string:place_id", methods=['PUT'],
 #                  strict_slashes=False)
