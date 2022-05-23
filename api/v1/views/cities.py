8791cb5044f6668a3708be66269d11e2c9063f66
andresh <ahenderson.holberton@gmail.com>
2022-05-23 09:12:04 -0300

    test 8

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 from api.v1.views import app_views
-from flask import Flask, Blueprint, render_template, jsonify, request, abort, make_response
+from flask import Flask, Blueprint, render_template, jsonify
+from flask import request, abort, make_response
 from models import storage
 from models.city import City
 from models.state import State
@@ -21,6 +22,7 @@ def show_all_cities_with_state_id(state_id):
         new_list.append(city.to_dict())
     return jsonify(new_list)
 
+
 @app_views.route("/cities/<string:city_id>", methods=['GET'],
                  strict_slashes=False)
 def show_city_with_id(city_id):
@@ -30,6 +32,7 @@ def show_city_with_id(city_id):
     if city is None:
         abort(404)
     return jsonify(city.to_dict())
+
 
 @app_views.route("/cities/<string:city_id>", methods=['DELETE'],
                  strict_slashes=False)
@@ -42,6 +45,7 @@ def delete_city_with_id(city_id):
     storage.delete(city)
     storage.save()
     return jsonify({}), 200
+
 
 @app_views.route("/states/<string:state_id>/cities", methods=['POST'],
                  strict_slashes=False)
@@ -60,6 +64,7 @@ def create_city_with_state_id(state_id):
     new_city.state_id = state_id
     new_city.save()
     return jsonify(new_city.to_dict()), 201
+
 
 @app_views.route("/cities/<string:city_id>", methods=['PUT'],
                  strict_slashes=False)
